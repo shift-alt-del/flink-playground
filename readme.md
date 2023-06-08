da6af8df00146c36fac9a35b654d196cd360d866
w-ding <w-ding@unext.jp>
2023-06-08 15:51:49 +0900

    Introduce word count.

--- readme.md
+++ readme.md
@@ -1,6 +1,8 @@
 # Flink playground
 
 
+## Playground setup
+
 Start docker-compose:
 
 ```
@@ -17,30 +19,31 @@ mvn compile assembly:single
 Create input topic and output topic
 
 ```
-docker exec -it broker kafka-topics --create \                                         
-    --bootstrap-server localhost:9092 \
-    --topic input-topic
-
-docker exec -it broker kafka-topics --create \                                         
-    --bootstrap-server localhost:9092 \
-    --topic output-topic
+kafka-topics --create --bootstrap-server localhost:9092 --topic input-topic
 ```
 
-Submit job:
+## Submit job:
 
 ```
 flink run -m localhost:18081 ./target/flink-etl-example-1.0.0-jar-with-dependencies.jar
 ```
 
-Input dummy data:
+## Insert dummy data:
 
-- Input one message in each line.
-- Press Ctrl+D to quite producer.
+- (Option1) Input one message in each line, press `Ctrl+D` to quite producer.
 
 ```
 kafka-console-producer --bootstrap-server localhost:9092 --topic input-topic
 ```
 
+- (Option2) Bulk insert.
+
+```
+kafka-console-producer --bootstrap-server localhost:9092 --topic input-topic < ./pom.xml
+```
+
+
+## Check output
 Check the output from flink:
 
 ```
--- src/main/java/FlinkETLJob.java
+++ src/main/java/FlinkETLJob.java
@@ -1,11 +1,14 @@
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
 
 public class FlinkETLJob {
 
@@ -21,8 +24,10 @@ public class FlinkETLJob {
                                 .setStartingOffsets(OffsetsInitializer.earliest())
                                 .setValueOnlyDeserializer(new SimpleStringSchema())
                                 .build(),
-                        WatermarkStrategy.noWatermarks(), "Kafka source")
-                .map(String::toUpperCase)
+                        WatermarkStrategy.noWatermarks(), "intput")
+                .flatMap(new Tokenizer()).name("tokenizer")
+                .keyBy((v) -> (v).f0).sum(1).name("counter")
+                .map((v) -> v.toString()).name("parse-string")
                 .sinkTo(KafkaSink.<String>builder()
                         .setBootstrapServers("broker:9092")
                         .setRecordSerializer(KafkaRecordSerializationSchema.builder()
@@ -30,9 +35,27 @@ public class FlinkETLJob {
                                 .setValueSerializationSchema(new SimpleStringSchema())
                                 .build())
                         .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
-                        .build());
+                        .build()).name("output");
 
         // Execute the Flink job
         env.execute("Flink ETL Job");
     }
+
+    public static final class Tokenizer
+            implements FlatMapFunction<String, Tuple2<String, Integer>> {
+
+        @Override
+        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
+            // normalize and split the line
+            String[] tokens = value.toLowerCase().split("\\W+");
+
+            // emit the pairs
+            for (String token : tokens) {
+                if (token.length() > 0) {
+                    out.collect(new Tuple2<>(token, 1));
+                }
+            }
+        }
+    }
+
 }
